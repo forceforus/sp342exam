c922863aefddd7c11b0ea60294088da9764bb1d9
Force <2110511101041@live4.utcc.ac.th>
2023-04-27 10:07:29 +0700

    national id is number and have length 13

--- validate.js
+++ validate.js
@@ -1,35 +1,37 @@
 function checkNID() {
-  let nid = (document.getElementById("nid").value).trim();
-  if (nid.length != 13) {
-    return false;
-  } else {
-	return true;
-  }
+    let nid = (document.getElementById("nid").value).trim();
+    if (!isNaN(nid)) {
+        if (nid.length != 13) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 function checkTicketNo() {
-  let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
-    return false;
-  } else {
-	return true;
-  }
+    let num = (document.getElementById("ticknum").value).trim();
+    if (isNaN(num)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-function validateForm(){
-	if(!checkNID()){
-	  alert("Invalid value for National ID!");
-	  document.getElementById("nid").focus();
-	  return false;
-	}else{
-		if(!checkTicketNo()){
-		  alert("Invalid value for No.of tickets!!");
-		  document.getElementById("ticknum").focus();
-		  return false;
-		}else{
-			total = priceCalculate();
-			alert("Total price for this booking is "+total+" USD");
-			return false;
-		}
-	}
+function validateForm() {
+    if (!checkNID()) {
+        alert("Invalid value for National ID!");
+        document.getElementById("nid").focus();
+        return false;
+    } else {
+        if (!checkTicketNo()) {
+            alert("Invalid value for No.of tickets!!");
+            document.getElementById("ticknum").focus();
+            return false;
+        } else {
+            total = priceCalculate();
+            alert("Total price for this booking is " + total + " USD");
+            return false;
+        }
+    }
 }
